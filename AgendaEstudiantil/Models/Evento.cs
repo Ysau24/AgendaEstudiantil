53d461c9fb3450d9a6e48077cdc0fb781730d3f4
Ysauri Jiménez <ysmajimo@gmail.com>
2025-08-07 21:44:04 -0400

    first commit

---
+++ AgendaEstudiantil/Models/Evento.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaEstudiantil.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+
+        [Required] 
+        public required string Titulo { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        public string? Descripcion { get; set; }
+    }
+}
