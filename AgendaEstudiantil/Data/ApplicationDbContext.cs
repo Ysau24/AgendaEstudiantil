311b04c4ea1ba172f3b62da56f7e70690939af6c
Ysauri Jiménez <ysmajimo@gmail.com>
2025-08-10 16:43:11 -0400

    Subiendo archivos

---
+++ AgendaEstudiantil/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AgendaEstudiantil.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaEstudiantil.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
---
+++ AgendaEstudiantil/Models/Evento.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaEstudiantil.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio")] 
+        public required string Titulo { get; set; }
+
+        [Required(ErrorMessage = "La fecha es obligatoria.")]
+        [DataType(DataType.Date)]
+        public DateTime Fecha { get; set; }
+
+        public string? Descripcion { get; set; }
+    }
+}
