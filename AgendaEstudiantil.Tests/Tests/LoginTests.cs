09882e01d038f31f903cc23c193a914457b5b7d9
Ysauri Jiménez <ysmajimo@gmail.com>
2025-08-10 16:55:03 -0400

    Quitar referencia de submódulo y añadir carpeta normal

---
+++ AgendaEstudiantil.Tests/Tests/LoginTests.cs
@@ -0,0 +1,34 @@
+﻿using AgendaEstudiantil.Tests.Helpers;
+
+
+namespace AgendaEstudiantil.Tests.Tests
+{
+    [TestClass]
+    public class LoginTests: BaseTest
+    {
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            CerrarNavegador();
+        }
+
+        [TestMethod]
+        public void Login_CaminoPositivo_UserCorrecto()
+        {
+            IniciarSesion();
+
+            string currentUrl = driver.Url;
+            Assert.IsTrue(currentUrl.Contains("/Eventos"), "No se redirigió correctamente después del login.");
+
+            GuardarEvidencia
+                (
+                    "Login_CaminoFeliz",
+                    "Reporte de Prueba Login Positiva",
+                    $"Login exitoso. Redirigido a <b>{currentUrl}</b>."
+                );
+
+        }
+
+    }
+}
